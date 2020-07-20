dd05b50f89ab3733b7ecaa766869d734ee242440
yishengxiaoyao <644875343@qq.com>
2020-07-20 20:50:55 +0800

    remove hiveudf

--- hiveudfdemo/src/main/java/com/edu/hive/GenericUDAFCollect.java
+++
@@ -1,9 +0,0 @@
-package com.edu.hive;
-
-import org.apache.hadoop.hive.ql.exec.Description;
-import org.apache.hadoop.hive.ql.udf.generic.AbstractGenericUDAFResolver;
-
-@Description(name = "collect")
-public class GenericUDAFCollect extends AbstractGenericUDAFResolver {
-
-}
